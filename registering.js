e8e9911fd5c3dcfaf88f99fabe4c0bd387ea37a6
manuelfernando <manuelcamaron@hotmail.com>
2017-07-08 08:07:22 -0500

    add example of registering channels

---
+++ registering.js
@@ -0,0 +1,18 @@
+...
+
+componentWillMount() {
+  this.pubnub.init(this);
+
+  this.pubnub.getStatus();
+
+  this.pubnub.getMessage('channel1');
+  this.pubnub.getMessage('channel2');
+  this.pubnub.getMessage('channelGroup1');
+
+  this.pubnub.getPresence('channel1');
+  this.pubnub.getPresence('channel2');
+
+  ...
+}
+
+...
